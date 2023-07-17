109f9c300dd193555df0ebaf3f362c2e66e8d93c
FRpeemapot <freezeosiris@gmail.com>
2023-07-17 18:29:03 +0700

    week4

---
+++ src/app/status/status.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-status',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './status.component.html',
+  styleUrls: ['./status.component.scss']
+})
+export class StatusComponent {
+  currentTime = Date();
+  userAgent = navigator.userAgent;
+  languages = navigator.languages;
+  cookies = navigator.cookieEnabled;
+  touches = navigator.maxTouchPoints;
+  logical = navigator.hardwareConcurrency;
+}
